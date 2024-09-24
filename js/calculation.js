0916504bd584713d890d9e1665da9d6b6b267feb
prayash231 <138357568+prayash231@users.noreply.github.com>
2024-09-24 21:09:57 +0600

    modal added in every donate button

--- js/calculation.js
+++ js/calculation.js
@@ -21,11 +21,12 @@ getId('donate-noakhali-btn').addEventListener('click', function(){
     const historyMsg = document.createElement('div');
     historyMsg.className = 'border border-[#1111111A] rounded-[2.5vw] lg:rounded-[0.833vw] w-[82vw] mt-[5vw] lg:m-[1.667vw] mx-auto lg:mx-0 space-y-5 py-[5vw] px-[5vw] lg:px-[1.667vw] lg:py-[1.667vw]';
     historyMsg.innerHTML = `
-        <h3> ${noakhaliInputValue} Taka is Donated for Flood Relief in Noakhali,Bangladesh</h3>
+        <h3 class="font-bold md:text-xl"> ${noakhaliInputValue} Taka is Donated for Flood Relief in Noakhali,Bangladesh</h3>
         <p>Date : ${new Date().toLocaleString()} (Bangladesh Standard Time)</p>
     `;
     const historySection = document.getElementById('history-section');
     historySection.insertBefore(historyMsg, historySection.firstChild);
+    my_modal_5.showModal();
 });
 
 // feni section calculation
@@ -51,11 +52,12 @@ getId('donate-feni-btn').addEventListener('click', function(){
     const historyMsg = document.createElement('div');
     historyMsg.className = 'border border-[#1111111A] rounded-[2.5vw] lg:rounded-[0.833vw] w-[82vw] mt-[5vw] lg:m-[1.667vw] mx-auto lg:mx-0 space-y-5 py-[5vw] px-[5vw] lg:px-[1.667vw] lg:py-[1.667vw]';
     historyMsg.innerHTML = `
-        <h3> ${feniInputValue} Taka is Donated for Flood Relief in Feni,Bangladesh</h3>
+        <h3 class="font-bold md:text-xl"> ${feniInputValue} Taka is Donated for Flood Relief in Feni,Bangladesh</h3>
         <p>Date : ${new Date().toLocaleString()} (Bangladesh Standard Time)</p>
     `;
     const historySection = document.getElementById('history-section');
     historySection.insertBefore(historyMsg, historySection.firstChild);
+    my_modal_5.showModal();
 });
 
 // quota section calculation
@@ -81,11 +83,12 @@ getId('donate-quota-btn').addEventListener('click', function(){
     const historyMsg = document.createElement('div');
     historyMsg.className = 'border border-[#1111111A] rounded-[2.5vw] lg:rounded-[0.833vw] w-[82vw] mt-[5vw] lg:m-[1.667vw] mx-auto lg:mx-0 space-y-5 py-[5vw] px-[5vw] lg:px-[1.667vw] lg:py-[1.667vw]';
     historyMsg.innerHTML = `
-        <h3> ${quotaInputValue} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
+        <h3 class="font-bold md:text-xl"> ${quotaInputValue} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
         <p>Date : ${new Date().toLocaleString()} (Bangladesh Standard Time)</p>
     `;
     const historySection = document.getElementById('history-section');
     historySection.insertBefore(historyMsg, historySection.firstChild);
+    my_modal_5.showModal();
 });
 
 
